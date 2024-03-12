3d6db69a2921682d2de5bf3a9287561b0fe24e27
DevTunechi <tunjidare2@yahoo.com>
2024-03-12 06:15:31 -0700

    Rows of the first_table

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,9 @@
+-- -- 6-list_rows.sql
+
+-- This script lists rows of the first_table in the specified database
+
+-- Specify the database name
+USE `mysql`;
+
+-- Select all fields from the first_table
+SELECT * FROM first_table;
