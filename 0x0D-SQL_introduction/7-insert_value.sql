8f67acff2dcbc5a0d744832168e377c64364059a
DevTunechi <tunjidare2@yahoo.com>
2024-03-12 06:24:15 -0700

    Inserts a new role in the first_table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,11 @@
+-- 7-insert_row.sql
+
+-- This script inserts a new row into the table first_table in the specified database
+
+-- Author: Olatunji Oluwadare
+
+-- Specify the database name
+USE `hbtn_0c_0`;
+
+-- Insert a new row into first_table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
