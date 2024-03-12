0cb9c7df3a837716e95c5609586bdd023025d4e6
DevTunechi <tunjidare2@yahoo.com>
2024-03-12 05:38:02 -0700

    List all tables of MySQL database

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,8 @@
+-- 3-list_tables.sql
+-- Author: Olatunji Oluwadare
+-- This script lists all tables in MySQL database
+-- Database name 
+USE mysql;
+
+-- Show all tables in the current database
+SHOW TABLES;
